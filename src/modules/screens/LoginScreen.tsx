4210fda80dffb436ca29aa4aad2d805ea47b0507
Robson Ribeiro <robson.riber777@gmail.com>
2023-12-18 14:31:20 -0300

    Aula 123 - criando react hook customizado - alert

--- src/modules/screens/LoginScreen.tsx
+++ src/modules/screens/LoginScreen.tsx
@@ -1,8 +1,9 @@
 import axios from "axios";
 import { useState } from "react";
 
-import Button from "../../shared/buttons/button/Button";
-import Input from "../../shared/inputs/input/input";
+import Button from "../../shared/components/buttons/button/Button";
+import Input from "../../shared/components/inputs/input/input";
+import { useRequests } from "../../shared/hooks/useRequests";
 import {
   BackgroundImage,
   ContainerLogin,
@@ -15,6 +16,7 @@ import {
 const LoginScreen = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const { postRequest, loading } = useRequests();
 
   const handleEmail = (event: React.ChangeEvent<HTMLInputElement>) => {
     setEmail(event.target.value);
@@ -24,24 +26,11 @@ const LoginScreen = () => {
     setPassword(event.target.value);
   };
 
-  const handleLogin = async () => {
-    await axios({
-      method: "post",
-      url: "http://localhost:8080/auth",
-      data: {
-        email: email,
-        password: password,
-      },
-    })
-      .then((result) => {
-        alert(`Login efetuado com sucesso! ${result.data.accessToken} `);
-        return result.data;
-      })
-      .catch(() => {
-        alert("Usuário e ou senha inválidos!");
-      });
-
-    //console.log("returnObject: ", returnObject);
+  const handleLogin = () => {
+    postRequest("http://localhost:8080/auth", {
+      email: email,
+      password: password,
+    });
   };
 
   return (
@@ -57,12 +46,14 @@ const LoginScreen = () => {
             value={email}
           />
           <Input
+            type="password"
             title="Senha"
             margem="10px 0px 0px 0px"
             onChange={handlePassword}
             value={password}
           />
           <Button
+            loading={loading}
             type="primary"
             margem="40px 0px 10px 0px"
             onClick={handleLogin}
